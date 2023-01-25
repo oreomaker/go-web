c5a49065eb6df44f3a4c97c4e3d7d7074f8d2d09
oreomaker <zh002919@outlook.com>
2023-01-25 10:41:49 +0800

    添加dal配置

--- user/main.go
+++ user/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	demouser "go-web/user/kitex_gen/demouser/userservice"
+	"go-web/api/pkg/mw"
+	"go-web/user/kitex_gen/demouser/userservice"
+	"go-web/user/pkg/consts"
 	"log"
+	"net"
+
+	"github.com/cloudwego/kitex/pkg/limit"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
+func Init() {
+	// dal.Init()
+}
+
 func main() {
-	svr := demouser.NewServer(new(UserServiceImpl))
+	r, err := etcd.NewEtcdRegistry([]string{consts.ETCDAddress})
+	if err != nil {
+		panic(err)
+	}
+	addr, err := net.ResolveTCPAddr(consts.TCP, consts.UserServiceAddr)
+	if err != nil {
+		panic(err)
+	}
+	Init()
 
-	err := svr.Run()
-
+	svr := userservice.NewServer(new(UserServiceImpl),
+		server.WithServiceAddr(addr),
+		server.WithRegistry(r),
+		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}),
+		server.WithMuxTransport(),
+		server.WithMiddleware(mw.CommonMiddleware),
+		server.WithMiddleware(mw.ServerMiddleware),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: consts.UserServiceName}),
+	)
+	err = svr.Run()
 	if err != nil {
 		log.Println(err.Error())
 	}
